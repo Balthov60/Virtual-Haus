168037c02898b75d0b942155dfc0f4a7f849fa0a
Balthov60 <Balthazarfrolin@gmail.com>
2018-02-22 13:35:38 +0100

    Rename Folder & Clean Code Network handling

---
+++ HTCSide/Assets/Scripts/HololensNetwork/ServerNetworkManager.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerNetworkManager : MonoBehaviour
+{
+    private static readonly int NETWORK_PORT = 4875;
+
+    private Transform currentAppartment;
+    private Transform player;
+
+    private Dictionary<int, bool> connectionsReady;
+
+    void Start()
+    {
+        currentAppartment = GameObject.Find("Appartment").transform;
+        player = GameObject.Find("Player").transform;
+
+        connectionsReady = new Dictionary<int, bool>();
+        SetupServer();
+    }
+    private void SetupServer()
+    {
+        NetworkServer.RegisterHandler(VirtualHausMessageType.CONNECTED, OnClientConnect);
+        NetworkServer.RegisterHandler(VirtualHausMessageType.APPARTMENT_LOADED, OnClientAppartmentLoaded);
+        NetworkServer.RegisterHandler(VirtualHausMessageType.USER_READY, OnClientReady);
+
+        NetworkServer.RegisterHandler(VirtualHausMessageType.NEW_FURNITURE_POSITION, UpdateFurniturePosition);
+        NetworkServer.RegisterHandler(MsgType.Disconnect, OnClientDisconnect);
+
+        NetworkServer.Listen(NETWORK_PORT);
+    }
+
+    void Update()
+    {
+        SendPlayerPosUpdate();
+    }
+
+
+    /*****************************/
+    /* Public Messages Interface */
+    /*****************************/
+
+    /* Setup client */
+
+    /// <summary>
+    /// When client connect to server :
+    /// Register him & send the current appartment informations. 
+    /// </summary>
+    /// <param name="netMsg"></param>
+    public void OnClientConnect(NetworkMessage netMsg)
+    {
+        connectionsReady.Add(netMsg.conn.connectionId, false);
+
+        AppartmentLoadingMessage appartmentLoadingMessage = new AppartmentLoadingMessage
+        {
+            modelName = PrefabUtility.GetPrefabParent(currentAppartment).name,
+            roomQuantity = GameObject.Find("Furnitures").transform.childCount,
+
+            appartmentScale = currentAppartment.localScale,
+            appartmentPosition = currentAppartment.position,
+        };
+
+        NetworkServer.SendToClient(netMsg.conn.connectionId, VirtualHausMessageType.APPARTMENT_LOADING, appartmentLoadingMessage);
+    }
+
+    /// <summary>
+    /// When client finished appartment loading :
+    /// Send him furnitures used for the current appartment.
+    /// 
+    /// Send this furnitures room by room to prevent oversized messages.
+    /// </summary>
+    /// <param name="netMsg"></param>
+    public void OnClientAppartmentLoaded(NetworkMessage netMsg)
+    {
+        for (int i = 0; i < GameObject.Find("Furnitures").transform.childCount; i++)
+        {
+            FurnitureLoadingMessage furnitureLoadingMessage = new FurnitureLoadingMessage
+            {
+                jsonFurnitures = JsonUtility.ToJson(new NewFurnituresInformations(i))
+            };
+
+            NetworkServer.SendToClient(netMsg.conn.connectionId, VirtualHausMessageType.FURNITURE_LOADING, furnitureLoadingMessage);
+        }
+    }
+
+    /// <summary>
+    /// Client is ready when all furnitures have been loaded.
+    /// </summary>
+    /// <param name="netMsg"></param>
+    public void OnClientReady(NetworkMessage netMsg)
+    {
+        connectionsReady[netMsg.conn.connectionId] = true;
+    }
+
+
+    /* Send update to client */
+
+    public void SendFurniturePosUpdate(GameObject furniture)
+    {
+        NewFurniturePositionMessage furniturePositionMessage = new NewFurniturePositionMessage()
+        {
+            furnitureName = furniture.transform.name,
+            furniturePosition = furniture.transform.position,
+            furnitureRotation = furniture.transform.rotation
+        };
+
+        SendMessageToAllClientReady(VirtualHausMessageType.NEW_FURNITURE_POSITION, furniturePositionMessage);
+    }
+    private void SendPlayerPosUpdate()
+    {
+        UserPositionMessage userPositionMessage = new UserPositionMessage()
+        {
+            userPosition = player.position,
+            userRotation = player.rotation
+        };
+
+        SendMessageToAllClientReady(VirtualHausMessageType.USER_POSITION, userPositionMessage);
+    }
+
+    private void SendMessageToAllClientReady(short msgType, MessageBase message)
+    {
+        // TODO: Fix this error (resuming game after a script update remove this connectionsReady GO)
+        if (connectionsReady == null) return;
+
+        foreach (KeyValuePair<int, bool> connection in connectionsReady)
+        {
+            if (connection.Value == true)
+            {
+                NetworkServer.SendToClient(connection.Key, msgType, message);
+            }
+        }
+    }
+
+    
+    /* Apply updates made my client */
+
+    public void UpdateFurniturePosition(NetworkMessage netMsg)
+    {
+        NewFurniturePositionMessage msg = netMsg.ReadMessage<NewFurniturePositionMessage>();
+
+        Transform toUpdate = GameObject.Find(msg.furnitureName).transform;
+        toUpdate.position = msg.furniturePosition;
+        toUpdate.rotation = msg.furnitureRotation;
+    }
+
+
+    /* End client connection */
+
+    public void OnClientDisconnect(NetworkMessage netMsg)
+    {
+        connectionsReady.Remove(netMsg.conn.connectionId);
+    }
+}
--- HololensSide/Assets/Scripts/ClientNetworkManager.cs
+++ HololensSide/Assets/Scripts/ClientNetworkManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Networking;
 
 public class ClientNetworkManager : MonoBehaviour {
@@ -10,79 +8,113 @@ public class ClientNetworkManager : MonoBehaviour {
 
     private NetworkClient client;
     private ClientStatus status;
+    private int roomQuantityToLoad;
 
     private Transform player;
 
-    void Start()
+    private void Start()
     {
         status = ClientStatus.DISCONNECTED;
-
         player = GameObject.Find("Player").transform;
-    }
 
-    void Update()
-    {
-        if (status == ClientStatus.DISCONNECTED)
-            SetupClient();
+        SetupClient();
     }
-
-    public void SetupClient()
+    private void SetupClient()
     {
         client = new NetworkClient();
         client.RegisterHandler(MsgType.Connect, OnConnected);
-        client.RegisterHandler(VirtualHausMessageTypes.APPARTMENT_LOADING, LoadAppartment);
-        client.RegisterHandler(VirtualHausMessageTypes.FURNITURE_LOADING, LoadFurnitures);
+        client.RegisterHandler(VirtualHausMessageType.APPARTMENT_LOADING, LoadAppartment);
+        client.RegisterHandler(VirtualHausMessageType.FURNITURE_LOADING, LoadFurnitures);
 
-        client.RegisterHandler(VirtualHausMessageTypes.USER_POSITION, UpdateUserPosition);
+        client.RegisterHandler(VirtualHausMessageType.USER_POSITION, UpdateUserPosition);
+        client.RegisterHandler(VirtualHausMessageType.NEW_FURNITURE_POSITION, UpdateFurniturePosition);
 
         client.Connect(NETWORK_IP_ADDRESS, NETWORK_PORT);
         status = ClientStatus.CONNECTING;
     }
 
+    /*****************************/
+    /* Public Messages Interface */
+    /*****************************/
+
+    /* Setup client */
+
     public void OnConnected(NetworkMessage netMsg)
     {
-        client.Send(VirtualHausMessageTypes.CONNECTED, new EmptyMessage());
+        client.Send(VirtualHausMessageType.CONNECTED, new EmptyMessage());
         status = ClientStatus.CONNECTED;
     }
+
     public void LoadAppartment(NetworkMessage netMsg)
     {
         AppartmentLoadingMessage msg = netMsg.ReadMessage<AppartmentLoadingMessage>();
 
-        GameObject appartment = Instantiate(Resources.Load<GameObject>("Appartments/" + msg.modelName + "/" + msg.modelName));
-        appartment.transform.position = msg.appartmentPosition;
+        GameObject appartmentPrefab = Resources.Load<GameObject>("Appartments/" + msg.modelName + "/" + msg.modelName);
+        roomQuantityToLoad = msg.roomQuantity;
+
+        GameObject appartment = Instantiate(appartmentPrefab, msg.appartmentPosition, Quaternion.identity);
         appartment.transform.localScale = msg.appartmentScale;
 
-        client.Send(VirtualHausMessageTypes.APPARTMENT_LOADED, new EmptyMessage());
         status = ClientStatus.APPARTMENT_LOADED;
+        client.Send(VirtualHausMessageType.APPARTMENT_LOADED, new EmptyMessage());
     }
 
     public void LoadFurnitures(NetworkMessage netMsg)
     {
         FurnitureLoadingMessage msg = netMsg.ReadMessage<FurnitureLoadingMessage>();
-        PlaceFurniture(JsonUtility.FromJson<NewFurnituresInformations>(msg.jsonFurnitures));
+        PlaceFurnitures(JsonUtility.FromJson<NewFurnituresInformations>(msg.jsonFurnitures));
 
-        client.Send(VirtualHausMessageTypes.USER_READY, new EmptyMessage());
-        status = ClientStatus.READY;
-    }
-    private void PlaceFurniture(NewFurnituresInformations furnitures)
-    {
-        foreach (NewFurnitureInformations furniture in furnitures.furnitures)
+        if (--roomQuantityToLoad == 0) // Receive furnitures room by room to prevent oversized messages.
         {
-            GameObject instance = Instantiate(Resources.Load<GameObject>("Furnitures/Prefab/" + furniture.prefabName));
+            client.Send(VirtualHausMessageType.USER_READY, new EmptyMessage());
+            status = ClientStatus.READY;
+        }
+    }
+    private void PlaceFurnitures(NewFurnituresInformations furnitures)
+    {
+        foreach (NewFurnitureInformations furniture in furnitures.list)
+        {
+            GameObject furniturePrefab = Resources.Load<GameObject>("Furnitures/Prefab/" + furniture.prefabName);
+
+            GameObject instance = Instantiate(furniturePrefab, furniture.furniturePosition, furniture.furnitureRotation);
             instance.name = furniture.furnitureName;
-            instance.transform.position = furniture.furniturePosition;
-            instance.transform.rotation = furniture.furnitureRotation;
         }
     }
 
-    private void UpdateUserPosition(NetworkMessage netMsg)
+    /* Apply updates made by server */
+
+    public void UpdateUserPosition(NetworkMessage netMsg)
     {
         UserPositionMessage msg = netMsg.ReadMessage<UserPositionMessage>();
 
         player.position = msg.userPosition;
         player.rotation = msg.userRotation;
     }
+    public void UpdateFurniturePosition(NetworkMessage netMsg)
+    {
+        NewFurniturePositionMessage msg = netMsg.ReadMessage<NewFurniturePositionMessage>();
 
+        Transform toUpdate = GameObject.Find(msg.furnitureName).transform;
+        toUpdate.position = msg.furniturePosition;
+        toUpdate.rotation = msg.furnitureRotation;
+    }
+
+    /* Send Update to server */
+    public void SendFurniturePosUpdate(GameObject furniture)
+    {
+        NewFurniturePositionMessage furniturePositionMessage = new NewFurniturePositionMessage()
+        {
+            furnitureName = furniture.transform.name,
+            furniturePosition = furniture.transform.position,
+            furnitureRotation = furniture.transform.rotation
+        };
+
+        client.Send(VirtualHausMessageType.NEW_FURNITURE_POSITION, furniturePositionMessage);
+    }
+
+    /*********************/
+    /* Utilities Methods */
+    /*********************/
 
     public enum ClientStatus
     {
