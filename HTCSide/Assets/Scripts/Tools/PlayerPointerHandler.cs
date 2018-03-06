70c5abeedf61b410e6659704c90fb7a94113fb2f
Balthov60 <Balthov60@gmail.com>
2018-03-06 14:12:31 +0100

    Fix Player Pointer Issues

---
+++ HTCSide/Assets/Scripts/Tools/PlayerPointerHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPointerHandler : MonoBehaviour
+{
+
+    public GameObject camera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newRotation = transform.rotation.eulerAngles;
+        newRotation.y = camera.transform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(newRotation);
+    }
+}
