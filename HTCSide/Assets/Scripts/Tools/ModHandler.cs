cdbad4f089ebc4029518b0a5b60dd752c07168d3
Balthov60 <Balthazarfrolin@gmail.com>
2018-03-10 22:38:15 +0100

    Link Tuto With Mod Switcher

--- HTCSide/Assets/Scripts/Tools/ModHandler.cs
+++ HTCSide/Assets/Scripts/Tools/ModHandler.cs
@@ -7,12 +7,23 @@ public class ModHandler : MonoBehaviour
     private GameObject furnitureMenu;
     private GameObject roomTpUi;
 
+    private GameObject removeModTuto;
+    private GameObject editionModTuto;
+    private GameObject teleportationModTuto;
+
     private void Start()
     {
         mod = Mod.EDITION;
         inputManager = GameObject.Find("InputManager").GetComponent<InputManager>();
         furnitureMenu = GameObject.Find("FurnitureMenu");
         roomTpUi = GameObject.Find("RoomTpUI");
+
+        removeModTuto = GameObject.Find("RemoveModTuto");
+        editionModTuto = GameObject.Find("EditionModTuto");
+        teleportationModTuto = GameObject.Find("TeleportationModTuto");
+
+        removeModTuto.SetActive(false);
+        teleportationModTuto.SetActive(false);
     }
 
     private void Update()
@@ -25,18 +36,30 @@ public class ModHandler : MonoBehaviour
                 mod = Mod.REMOVE;
                 furnitureMenu.SetActive(false);
                 roomTpUi.SetActive(false);
+
+                removeModTuto.SetActive(true);
+                editionModTuto.SetActive(false);
+                teleportationModTuto.SetActive(false);
             }
             else if (menuTrackPadPos.x > 0 && menuTrackPadPos.y < 0)
             {
                 mod = Mod.UTILITIES;
                 furnitureMenu.SetActive(false);
                 roomTpUi.SetActive(true);
+
+                removeModTuto.SetActive(false);
+                editionModTuto.SetActive(false);
+                teleportationModTuto.SetActive(true);
             }
             else
             {
                 mod = Mod.EDITION;
                 furnitureMenu.SetActive(true);
                 roomTpUi.SetActive(false);
+
+                removeModTuto.SetActive(false);
+                editionModTuto.SetActive(true);
+                teleportationModTuto.SetActive(false);
             }
         }
 
@@ -47,14 +70,26 @@ public class ModHandler : MonoBehaviour
         if (Input.GetKey(KeyCode.I))
         {
             mod = Mod.EDITION;
+
+            removeModTuto.SetActive(false);
+            editionModTuto.SetActive(true);
+            teleportationModTuto.SetActive(false);
         }
         else if (Input.GetKey(KeyCode.O))
         {
             mod = Mod.UTILITIES;
+
+            removeModTuto.SetActive(false);
+            editionModTuto.SetActive(false);
+            teleportationModTuto.SetActive(true);
         }
         else if (Input.GetKey(KeyCode.P))
         {
             mod = Mod.REMOVE;
+
+            removeModTuto.SetActive(true);
+            editionModTuto.SetActive(false);
+            teleportationModTuto.SetActive(false);
         }
     }
 
