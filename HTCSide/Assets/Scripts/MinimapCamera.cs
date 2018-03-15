ec9ba2b2f3f03fb9bedd6b1c536083ab45fe1ee6
Balthov60 <Balthov60@gmail.com>
2018-03-15 14:55:23 +0100

    Fix Minimap Issue

---
+++ HTCSide/Assets/Scripts/MinimapCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour
+{
+    public Shader unlitShader;
+
+    void Start()
+    {
+        GetComponent<Camera>().SetReplacementShader(unlitShader, "");
+    }
+}
--- HTCSide/Assets/Scripts/Tools/PlayerPointerHandler.cs
+++ HTCSide/Assets/Scripts/Tools/PlayerPointerHandler.cs
@@ -7,11 +7,12 @@ public class PlayerPointerHandler : MonoBehaviour
 
     public GameObject camera;
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 newRotation = transform.rotation.eulerAngles;
         newRotation.y = camera.transform.rotation.eulerAngles.y;
         transform.rotation = Quaternion.Euler(newRotation);
+
+        transform.localPosition = camera.transform.localPosition = Vector3.zero;
     }
 }
